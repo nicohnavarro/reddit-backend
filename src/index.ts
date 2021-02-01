a972535d3f55a8476aba5e01b8239a4375fd9350
nicohnavarro <nicohnavarro@gmail.com>
2021-02-01 02:14:54 -0300

    :fire: add first graphql resolver!

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,37 @@ import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 import mikroOrmConfig from './mikro-orm.config';
-
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import {buildSchema} from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
-  await orm.getMigrator().up();
+  //await orm.getMigrator().up();
+
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate:false,
+    })
+  })
+
+  apolloServer.applyMiddleware({ app });
+
+  app.get('/', (req,res)=>{
+    res.send("Hello");
+  });
+
+  app.listen(4000, ()=> {
+    console.log('server on port 4000')
+  });
+
   //const post = orm.em.create(Post, { title: 'First post ' });
   //await orm.em.persistAndFlush(post);
   //await orm.em.nativeInsert(Post, { title: 'post 2 ' })
-  const posts = await orm.em.find(Post,{});
-  console.log(posts);
+  //const posts = await orm.em.find(Post,{});
+  //console.log(posts);
 }
 
 console.log("hello world");
